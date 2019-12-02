16721b55496219b5811999bcf0dcde5083739608
Guy Steinberg <guysteinberg123@gmail.com>
2019-12-02 09:11:30 +0200

    Fixed Dependencies - Recycler View

--- app/src/main/java/com/example/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/popularmoviesstage1/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.popularmoviesstage1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_movies);
+
+        //initiazlise Recycler view
+        //set layout manager, set fixed size, set adapter
     }
 }
