c60b26615ced7513a073f2112ab477356b77dc8d
Guy Steinberg <guysteinberg123@gmail.com>
2019-12-03 06:21:26 +0200

    Added Recyclyer View Adapter
    Created adapter + view holder for movies
    initialized the adapter in main activity

--- app/src/main/java/com/example/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/popularmoviesstage1/MainActivity.java
@@ -5,25 +5,34 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
+    private MoviePostersAdapter mMovieAdapter;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_movies);
+        mRecyclerView = (RecyclerView) findViewById(R.id.movies_rv);
 
         //initiazlise Recycler view
         //set layout manager, set fixed size, set adapter
+        mMovieAdapter = new MoviePostersAdapter();
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setAdapter(mMovieAdapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setHasFixedSize(true); //movie posters will always have same size
+
+        
 
 
     }
--- app/src/main/java/com/example/popularmoviesstage1/MoviePostersAdapter.java
+++ app/src/main/java/com/example/popularmoviesstage1/MoviePostersAdapter.java
@@ -1,6 +1,11 @@
 package com.example.popularmoviesstage1;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,21 +13,49 @@ import androidx.recyclerview.widget.RecyclerView;
 //adapter to handle the recylcler view
 public class MoviePostersAdapter extends RecyclerView.Adapter<MoviePostersAdapter.MoviePostersViewHolder> {
 
+    private final String TAG = "MoviePostesAdapter";
+    private int nummovies = 0;
     private Movie[] movies;
+
+    public MoviePostersAdapter() {
+    }
+
+
+    //view holder to handle each child element of the recycleView
+    public class MoviePostersViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mMovieTitleTV;
+
+        public MoviePostersViewHolder(View view) {
+            super(view);
+
+            mMovieTitleTV = (TextView) view.findViewById(R.id.movie_card_title);
+
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public MoviePostersAdapter.MoviePostersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder: initializing view holder");
+        //inflate layout
+        Context context = parent.getContext();
+        int layoutID = R.layout.movie_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(layoutID, parent, false);
+        return new MoviePostersViewHolder(view);
+    }
 
     @Override
     public void onBindViewHolder(@NonNull MoviePostersViewHolder holder, int position) {
 
     }
 
-    @NonNull
     @Override
-    public MoviePostersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    //view holder to handle each child elemebnt of the recycleView
-    public class MoviePostersViewHolder extends RecyclerView.ViewHolder {
-
+    public int getItemCount() {
+        if (movies == null) return 0;
+        return movies.length;
     }
 }
